13748059b8e1ec3b2f1e6f6094de06b19f448661
Senthilstayss <senthil.dhakshna@gmail.com>
2023-02-05 13:35:11 +0530

    latest changes added helpbox

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h4>{title}</h4>
       <p>{text}</p>
     </article>
   );
